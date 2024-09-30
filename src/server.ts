5c629592976f647c7c7af0f87d566de35b2c49b9
Cleyber <cleyber.fernandes@gmail.com>
2024-09-30 13:33:24 -0300

    rendering the page

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,12 @@ import dotenv from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
 import router from "./routes";
-import * as AppController from "./controllers/AppController";
 dotenv.config();
 
 const app = express();
 
+
+app.use(express.static(path.join(__dirname, "../public")));
 app.set("view engine", "mustache");
 app.set("views", path.join(__dirname, "views"));
 app.engine("mustache", mustache());
@@ -16,7 +17,5 @@ app.use(router);
 app.use((req, res) => {
     res.send("Página não encontrada");
 });
-
-app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(process.env.PORT);
