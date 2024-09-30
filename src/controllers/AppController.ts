22ac97a02d3a1e7e2c05fbfcccb59198c426e7df
Cleyber <cleyber.fernandes@gmail.com>
2024-09-30 14:25:52 -0300

    Passing data to the view and changing the active menu button

--- src/controllers/AppController.ts
+++ src/controllers/AppController.ts
@@ -1,19 +1,44 @@
 import { Request, Response } from "express";
+import { createMenuObject } from "../helpers/activeMenu";
 
 export const home = (req: Request, res: Response) => {
-    res.render("pages/page");
+    res.render("pages/page", {
+        menu: createMenuObject("all"),
+        banner: {
+            title: "Todos os animais",
+            image: "allanimals.jpg"
+        }
+    });
 }
 
 export const dogs = (req: Request, res: Response) => {
-
+    res.render("pages/page", {
+        menu: createMenuObject("dog"),
+        banner: {
+            title: "Cachorros",
+            image: "banner_dog.jpg"
+        }
+    });
 }
 
 export const cats = (req: Request, res: Response) => {
-
+    res.render("pages/page", {
+        menu: createMenuObject("cat"),
+        banner: {
+            title: "Gatos",
+            image: "banner_cat.jpg"
+        }
+    });
 }
 
 export const fishes = (req: Request, res: Response) => {
-
+    res.render("pages/page", {
+        menu: createMenuObject("fish"),
+        banner: {
+            title: "Peixes",
+            image: "banner_fish.jpg"
+        }
+    });
 }
 
 export const search = (req: Request, res: Response) => {
